2522d975ce2c455260da6d183e0760a49572c036
Fion1x <nikitakravcov247@gmail.com>
2025-11-10 11:47:59 +0700

    toolbar

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import sys
+from pathlib import Path
 from PyQt6.QtWidgets import QApplication
 from main_window import MainWindow
 
+def dir_pictures():
+    app_dir = Path.home() / ".dir_pictures"
+    app_dir.mkdir(parents = True, exist_ok = True)
+    return app_dir
 
 if __name__ == "__main__":
+    dir_pictures()
+
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
--- main_window.py
+++ main_window.py
@@ -1,9 +1,29 @@
 import sys
-from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtWidgets import QToolBar, QSizePolicy, QMainWindow, QWidget, QVBoxLayout, QPushButton, QHBoxLayout
+from PyQt6.QtCore import Qt
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.resize(500, 300)
 
+        toolbar = QToolBar("Top", self)
+        self.addToolBar(Qt.ToolBarArea.TopToolBarArea, toolbar)
 
+        bar = QWidget()
+        bar.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred)
+
+        row = QHBoxLayout(bar)
+        row.setContentsMargins(0,0,0,0)
+        row.setSpacing(0)
+
+
+        btn_add = QPushButton("+")
+        btn_1   = QPushButton("?")
+        btn_2   = QPushButton("*") 
+
+        for b in (btn_add, btn_1, btn_2):
+            b.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred)
+            row.addWidget(b, 1)   # одинаковый stretch-фактор
+
+        toolbar.addWidget(bar)
